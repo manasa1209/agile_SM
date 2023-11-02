a34b9924b64f87fbb6d6d83268e6d3375dedf3a4
manasa1209 <smnavada@gmail.com>
2023-11-02 17:45:45 +0530

    version 1

---
+++ file1.py
@@ -0,0 +1,10 @@
+"""
+This is a simple Python script that adds two numbers.
+"""
+
+A = 1  # Use UPPER_CASE for constants
+B = 2  # Use UPPER_CASE for constants
+RESULT = A + B
+print(RESULT)
+
+# Ensure there is a final newline at the end of the file.
